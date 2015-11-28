9c5c74edbc262a11f3e764580b540cfb8860c578
ryuichi7 <brettrheenan@gmail.com>
2015-11-28 15:53:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,37 @@
+class Dog
+
+  def name
+    @name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+end
+
+class Person
+
+  def name
+    @name
+  end
+
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(job_name)
+    @job = job_name
+  end
+end
